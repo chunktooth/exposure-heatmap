3688e13194b1dbee3b6bf97629da5b5741df6c40
chunktooth <om.saetehn@gmail.com>
2020-04-10 18:01:08 -0600

    Dynamically rendering Features from sampleBatchMap.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-/* eslint-disable react/style-prop-object */
 import React from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+import ReactMapboxGl, { 
+  Layer, 
+  Feature,
+  // Image 
+} from 'react-mapbox-gl';
+// import batchGeoJSON from './exposureData/sampleBatchMap.geojson';
+import sampleBatchMap from './exposureData/sampleBatchMap.json';
 import './App.scss';
 
 const Map = ReactMapboxGl({
@@ -10,40 +16,56 @@ const Map = ReactMapboxGl({
 class App extends React.Component {
   constructor() {
     super();
-      this.state = {
-        center: [5.3753, 35],
-        zoom: [1.49],
-        style: "mapbox://styles/mapbox/dark-v10"
+    this.state = {
+      center: [5.3753, 35],
+      zoom: [1.49],
+      style: "mapbox://styles/mapbox/dark-v10",
+      exposureMap: sampleBatchMap
+    }
+  }
+
+  renderFeature = () => {
+    const { exposureMap } = this.state;
+
+    return exposureMap.features.map(ft => {
+      let coords = ft.geometry.coordinates;
+      // let imagePath = 'https://cdn.iconscout.com/icon/premium/png-256-thumb/pug-563422.png';
+      if(coords) {
+        return <Feature coordinates={coords}/>
+        // return <Image 
+        //   id={'exposure'} 
+        //   url={imagePath} 
+        //   data={batchGeoJSON}  
+        // />
       }
-  } 
+
+      return null;
+    })
+  }
 
   render() {
     const { style, zoom, center } = this.state;
 
-    return (
-      <Map
-        style={style}
-        zoom={zoom}
-        center={center}
-        containerStyle={{
-          height: '100vh',
-          width: '100vw'
-        }}>
-        <Layer 
-          type="symbol" 
-          id="marker" 
-          layout={{ 'icon-image': 'marker-15' }}>
-            <Feature 
-              coordinates={
-                [-0.481747846041145, 51.3233379650232]
-              }/>
-            <Feature 
-              coordinates={
-                [32, 51]
-              }/>
-        </Layer>
-      </Map>
-    );
+    if(this.state.exposureMap)  {
+      return (
+        <Map
+          style={style}
+          zoom={zoom}
+          center={center}
+          containerStyle={{
+            height: '100vh',
+            width: '100vw'
+          }}
+          interactive={true}>
+          <Layer 
+            type="symbol" 
+            id="marker" 
+            layout={{ 'icon-image': 'marker-15' }}> 
+            { this.renderFeature() }
+          </Layer>
+        </Map>
+      );
+    }
   }
 }
  
