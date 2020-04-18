e254dd575da4da24c586cbcab62a23328095b9a1
chunktooth <om.saetehn@gmail.com>
2020-04-17 20:16:13 -0600

    Added .gitignore and onto popup rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import React from 'react';
-import sampleBatchMap from './exposureData/sampleBatchMap.geojson';
-import ReactMapGL, { Source, Layer } from 'react-map-gl';
+import sampleBatchMap from './sampleData/sampleBatchMap.geojson';
+import exposureData from './sampleData/sampleBatchMap.json';
+import ReactMapGL, { 
+  Source, 
+  Layer, 
+  // Marker, 
+  // Popup 
+} from 'react-map-gl';
+import { token } from './sampleData/token';
+
+// const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+//   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+//   C20.1,15.8,20.2,15.8,20.2,15.7z`;
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      mapboxApiAccessToken: 'pk.eyJ1Ijoib3NhZXRlaG4iLCJhIjoiY2s4a3FraWwyMDRtdTNucW82bGswcXB6biJ9.Ks3reY0Gor-GqRVP9woI8Q',
+      mapboxApiAccessToken: token,
       viewport: {
         width: '100vw',
         height: '100vh',
         latitude: 5.3753,
         longitude: 35,
         zoom: 1.49
-        // style: "mapbox://styles/mapbox/dark-v10",
-      }
+      },
+      popupInfo: undefined,
+      exposureData: exposureData
     }
   }
+
+  // displayPopupModal = () => {
+  //   const { popupInfo } = this.state;
+    
+  //   return popupInfo &&
+  //     <Popup
+  //       tipSize={5}
+  //       anchor="top"
+  //       longitude={popupInfo.longitude}
+  //       latitude={popupInfo.latitude}
+  //       closeOnClick={false}
+  //       onClose={() => this.setState({popupInfo: null})}>
+  //       <p>{popupInfo.latitude + popupInfo.longitude}</p>
+  //     </Popup>;
+  // }
+
+  // onLayerHover = (ft) => {
+  //   let data = ft;
+  //   this.setState({ popupInfo: data });
+  // }
+
+  // displayMarker = () => {
+  //   return exposureData.features.map((ft, idx) => {
+  //     // let addressLine = ft.properties.AddressLine;
+  //     let coords = ft.geometry.coordinates;
+  //     let size = 20;
+
+  //     return <React.Fragment>
+  //       <Marker 
+  //         key={`marker-${idx}`} 
+  //         coordinates={coords}> 
+
+  //           <svg
+  //             height={size}
+  //             viewBox="0 0 24 24"
+  //             style={{
+  //               cursor: 'pointer',
+  //               fill: '#d00',
+  //               stroke: 'none',
+  //               transform: `translate(${-size / 2}px,${-size}px)`
+  //             }}
+  //             onMouseEnter={() => this.onLayerHover(ft)}>
+  //             <path d={ICON} />
+  //           </svg>
+  //       </Marker>
+
+  //       { this.displayPopupModal() }
+  //     </React.Fragment>
+  //   })
+  // }
 
   render() {
     const clusterLayer = {
@@ -56,22 +118,26 @@ class App extends React.Component {
       }
     };
 
-
     return (
       <ReactMapGL {...this.state.viewport}
         onViewportChange={(viewport) => this.setState({viewport})}
+        mapStyle='mapbox://styles/mapbox/dark-v10'
         mapboxApiAccessToken={this.state.mapboxApiAccessToken}>
+        
+        { this.displayMarker() }
         <Source
           type="geojson"
           data={sampleBatchMap}
           cluster={true}
           clusterMaxZoom={14}
-          clusterRadius={50}
+          clusterRadius={20}
           ref={this._sourceRef}>
+
           <Layer {...clusterLayer} />
           <Layer {...clusterCountLayer} />
           <Layer {...unclusteredPointLayer} />
         </Source>
+
       </ReactMapGL>
     )
   }
