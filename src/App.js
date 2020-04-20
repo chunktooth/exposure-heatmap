3bbd42dbd5521a4e801cb5827bb6a81bd88d97e8
chunktooth <om.saetehn@gmail.com>
2020-04-20 11:14:10 -0600

    Restructured directories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import sampleBatchMap from './sampleData/sampleBatchMap.geojson';
+import { 
+  Map as LeafletMap,
+  Marker,
+  Popup,
+  GeoJSON
+} from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      center: [51.505, -0.09],
+      zoom: 4
+    }
+  }
+
+  onViewPortChanged = (viewport) => {
+    return this.setState({ viewport })
+  }
+
+  render() {
+    const { center, zoom } = this.state;
+    const basemap = `https://a.tile.openstreetmap.org/${zoom}/${400}/${400}.png`;
+
+    return (
+      <LeafletMap 
+        center={center} 
+        zoom={zoom}
+        // style={}
+        // fitBounds={}
+        doubleClickZoom={true}
+        dragging={true}
+        // viewport={viewport}
+        // onViewportChange={this.onViewPortChanged}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <GeoJSON
+          data={sampleBatchMap}
+          style={() => ({
+            color: '#4a83ec',
+            weight: 0.5,
+            fillColor: "#1a1d62",
+            fillOpacity: 1,
+          })}
+        />
+        <Marker position={[50, 10]}>
+          <Popup>
+            Popup for any custom information.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
 }
+
 
 export default App;
